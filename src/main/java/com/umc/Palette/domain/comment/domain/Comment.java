d69739c579abf859510a9d8ad6f5bbdfbb7b7aa0
woohyuk <7dngur7@naver.com>
2024-01-16 19:50:09 +0900

    feat:Comment domain 클래스 작성

---
+++ src/main/java/com/umc/Palette/domain/comment/domain/Comment.java
@@ -0,0 +1,43 @@
+package com.umc.Palette.domain.comment.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+
+    @Column(name = "content")
+    private String content;
+
+//    @Column(name = "user_id")
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User user;
+
+//    @Column(name = "post_id")
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Post post;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
