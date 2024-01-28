6ff2a3f7da4c766aaecd55954049bc8c940943f7
woohyuk <7dngur7@naver.com>
2024-01-28 11:12:13 +0900

    feat: Comment 컨트롤러 수정

--- src/main/java/com/umc/Palette/domain/comment/controller/CommentRestController.java
+++ src/main/java/com/umc/Palette/domain/comment/controller/CommentRestController.java
@@ -4,6 +4,8 @@ import com.umc.Palette.domain.comment.dto.CommentDto;
 import com.umc.Palette.domain.comment.dto.CommentRequestDTO;
 import com.umc.Palette.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,23 +20,27 @@ public class CommentRestController {
     private final CommentService commentService;
 
     @GetMapping
-    public List<CommentDto> getComment(@PathVariable(name = "postId") Long postId){
-        return commentService.getComments(postId);
+    public ResponseEntity<List<CommentDto>> getComment(@PathVariable(name = "postId") Long postId) {
+        List<CommentDto> comments = commentService.getComments(postId);
+        return ResponseEntity.ok(comments);
     }
 
     @PostMapping
-    public CommentDto postComment(@RequestBody @Validated CommentRequestDTO request){
-        return commentService.createComment(request);
+    public ResponseEntity<CommentDto> postComment(@RequestBody @Validated CommentRequestDTO.CreateDTO request) {
+        CommentDto createdComment = commentService.createComment(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdComment);
     }
 
     @PatchMapping("/{commentId}")
-    public CommentDto patchComment(@PathVariable(name = "commentId") Long commentId, @RequestBody @Validated CommentRequestDTO request){
-        return commentService.updateComment(commentId, request);
+    public ResponseEntity<CommentDto> patchComment(@PathVariable(name = "commentId") Long commentId, @RequestBody @Validated CommentRequestDTO.CreateDTO request) {
+        CommentDto updatedComment = commentService.updateComment(commentId, request);
+        return ResponseEntity.ok(updatedComment);
     }
 
     @DeleteMapping("/{commentId}")
-    public boolean deleteComment(@PathVariable(name = "commentId") Long commentId){
-        return commentService.deleteComment(commentId);
+    public ResponseEntity<Boolean> deleteComment(@PathVariable(name = "commentId") Long commentId) {
+        boolean isDeleted = commentService.deleteComment(commentId);
+        return ResponseEntity.ok(isDeleted);
     }
 
     @GetMapping("/{commentId}/liked-people")
