459be1698d74be6b8af793b07d6310e4092d4bfe
woohyuk <7dngur7@naver.com>
2024-01-28 11:13:44 +0900

    feat: Comment 컨버터 추가

--- src/main/java/com/umc/Palette/domain/comment/converter/CommentConverter.java
+++ src/main/java/com/umc/Palette/domain/comment/converter/CommentConverter.java
@@ -1,20 +1,41 @@
 package com.umc.Palette.domain.comment.converter;
 
 import com.umc.Palette.domain.comment.domain.Comment;
+import com.umc.Palette.domain.comment.dto.CommentDto;
 import com.umc.Palette.domain.comment.dto.CommentRequestDTO;
 
 
 public class CommentConverter {
     public static Comment toComment(CommentRequestDTO.CreateDTO createDTO){
-
         //id 어떻게할건지
-        Comment newComment = Comment.builder()
+        return Comment.builder()
                 .content(createDTO.getComment())
                 .user(createDTO.getUserId())
                 .post(createDTO.getPostId())
                 .createdAt(createDTO.getCreatedAt())
                 .updatedAt(null).build();
-        return  newComment;
+    }
 
+    public static Comment toComment(Long commentID, CommentRequestDTO.CreateDTO createDTO){
+        //id 어떻게할건지
+        return Comment.builder()
+                .id(commentID)
+                .content(createDTO.getComment())
+                .user(createDTO.getUserId())
+                .post(createDTO.getPostId())
+                .createdAt(createDTO.getCreatedAt())
+                .updatedAt(null).build();
+    }
+
+
+
+    public static CommentDto toCommentDto(Comment comment){
+        return CommentDto.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .likeCount(comment.getLikes().size())
+                .build();
     }
 }
