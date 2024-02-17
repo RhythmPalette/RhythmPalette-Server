8793f77ea760bbf2285945b65eff9c8fa150b26b
woohyuk <7dngur7@naver.com>
2024-02-17 16:54:04 +0900

    fix: cicd start.sh파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 # start.sh
 
 PROJECT_ROOT="/home/ec2-user/deploy"
-JAR_FILE="$PROJECT_ROOT/server-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/Palette-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -11,7 +11,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT/
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
@@ -19,4 +19,4 @@ source ~/.bashrc
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURENT_PID 입니다." >> $DEPLOY_LOG
